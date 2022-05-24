4870997e5345d87457597d60dcd3f92e1b6b0954
shanu agrawal <agrawalshanu798@gmail.com>
2022-05-24 14:37:39 +0530

    feat: create Feed card

--- src/comps/FeedBar/FeedBar.jsx
+++ src/comps/FeedBar/FeedBar.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import * as React from "react";
+import Box from "@mui/material/Box";
+import { FeedCard } from "./FeedCard";
 
-const FeedBar = () => {
+function FeedBar() {
+
+
   return (
-    <div>FeedBar</div>
-  )
+    <>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "1rem",
+          marginTop: "3.5rem",
+        }}
+      >
+        <FeedCard/>
+      </Box>
+    </>
+  );
 }
 
-export {FeedBar}
+export { FeedBar };
